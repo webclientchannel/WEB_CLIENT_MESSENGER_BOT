05b6bc94a0cb68fd63db9f4050999002f025bc5c
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-07-28 00:55:15 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,7 +1,7 @@
 module.exports = {
  config: {
  name: "out",
- author: "xnil",
+ author: "🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝘾𝙝𝙖𝙣𝙣𝙚𝙡_🫩",
  role: 2, 
  shortDescription: "Make the bot leave the group",
  category: "admin",
@@ -14,10 +14,10 @@ module.exports = {
  // Check if it's a group chat
  const threadInfo = await api.getThreadInfo(threadID);
  if (!threadInfo.isGroup) {
- return api.sendMessage("❌ This command can only be used in group chats.", threadID);
+ return api.sendMessage("Apni parben na ey command use korte🦆💨", threadID);
  }
 
- await api.sendMessage("👋 Goodbye! I'm leaving this group now...", threadID, () => {
+ await api.sendMessage("🎀Allah Hafez all✨\n\n..Ami ey GROUP e thakar joggo na..😔", threadID, () => {
  api.removeUserFromGroup(api.getCurrentUserID(), threadID);
  });
  }
