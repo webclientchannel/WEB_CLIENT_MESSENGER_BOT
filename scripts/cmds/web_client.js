7749cbdf4ff9a037ccd8c9633af1ed4566238bd5
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-07-28 01:22:52 +0600

    Create web_client.js

---
+++ scripts/cmds/web_client.js
@@ -0,0 +1,38 @@
+module.exports.config = {
+  name: "xass",
+  version: 0.2,
+  author: "🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝘾𝙝𝙖𝙣𝙣𝙚𝙡_🫩",
+  category: "npx",
+  description: "🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝐁𝐎𝐓☠️",
+  countdown: 5,
+  role: 0,
+};
+
+module.exports.onStart = ({}) => {};
+
+module.exports.onChat = async ({ api, event, args }) => {
+  try {
+    const msg = event.body.toLowerCase();
+
+    if (msg === "webclient" || msg === "webbot") {
+      api.sendMessage(
+        {
+          body: 
+ `┏━━━✦✗✦━━━┓
+🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝐁𝐎𝐓☠️  
+  ┗━━━✦✗✦━━━┛
+  
+ > Owner: 🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝘾𝙝𝙖𝙣𝙣𝙚𝙡_🫩 
+ > 🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝐁𝐎𝐓☠️
+ Apni o ki chan? bot banate??
+ tayle type koren How can make bot/How to make bot
+`,
+        },
+        event.threadID,
+        event.messageID
+      );
+    }
+  } catch (err) {
+    api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
+  }
+};
