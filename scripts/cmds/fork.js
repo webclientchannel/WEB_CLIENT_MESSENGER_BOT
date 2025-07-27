307c775e21256ba8593ad03be939c036746a5caa
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-07-28 01:27:26 +0600

    Create fork.js

---
+++ scripts/cmds/fork.js
@@ -0,0 +1,35 @@
+module.exports.config = {
+  name: "xass",
+  version: 0.2,
+  author: "🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝘾𝙝𝙖𝙣𝙣𝙚𝙡_🫩",
+  category: "npx",
+  description: "🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝐁𝐎𝐓☠️ FORK",
+  countdown: 5,
+  role: 0,
+};
+
+module.exports.onStart = ({}) => {};
+
+module.exports.onChat = async ({ api, event, args }) => {
+  try {
+    const msg = event.body.toLowerCase();
+
+    if (msg === "/fork" || msg === "fork") {
+      api.sendMessage(
+        {
+          body: 
+ `Eyta 🎀ᵂᴱᴮ_𝐂𝐥𝐢𝐞𝐧𝐭🌐_𝐁𝐎𝐓☠️ Bot er fork eta dye apni o banai felen bot🧠😉
+ 
+  ┏━━━━━━━━━━━┓
+ 
+  ┗━━━━━━━━━━━┛
+`,
+        },
+        event.threadID,
+        event.messageID
+      );
+    }
+  } catch (err) {
+    api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
+  }
+};
