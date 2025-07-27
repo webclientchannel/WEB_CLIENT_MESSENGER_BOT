d130616bdc8174ebcf544bde8263009cb2d3410b
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-07-28 00:37:15 +0600

    Update catbox.js

--- scripts/cmds/catbox.js
+++ scripts/cmds/catbox.js
@@ -26,7 +26,7 @@ module.exports.onStart = async ({ api, event }) => {
   try {
    const allUrl = event.messageReply?.attachments[0]?.url; 
    if (!allUrl) {
-        return api.sendMessage("Ekta image ba video diye reply den ${prefix} catbox jate sheta uplode dite pari🎀", event.threadID, event.messageID);
+        return api.sendMessage("Ekta image ba video diye reply den ${prefix}catbox jate sheta uplode dite pari🎀", event.threadID, event.messageID);
       };
    const msg = await api.sendMessage("✨ Apnar attachment Uploading Hocche.. Kichukkhon opekkha koren✨", event.threadID);
 
